8f118357857124fc605332e61c653a15ed087dba
Elimists <pandey.pran@gmail.com>
2020-06-20 15:38:19 -0600

    Grouped the export const and added some new colours

--- constants/colors.js
+++ constants/colors.js
@@ -1,2 +1,8 @@
-export const  PRIMARY_COLOR = '#66BEFD';
-export const  LIGHT_GREY = '#CFCFCF';
+
+export const {
+    PRIMARY_COLOR           = '#66BEFD',
+    LIGHT_GREY              = '#CFCFCF',
+    LOGIN_BUTTON_COLOR      = '#5cb85c',
+    LOGOUT_BUTTON_COLOR     = '#d9534f',
+    SIGNUP_BUTTON_COLOR     = '#0275d8',
+}
