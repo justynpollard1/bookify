900546eb2c46d697153da43c8efe71f5034eda48
Elimists <pandey.pran@gmail.com>
2020-06-20 15:40:04 -0600

    Organized the constant colors.js

--- constants/colors.js
+++ constants/colors.js
@@ -1,8 +1,14 @@
-
 export const {
+
+    // LAYOUT COLORS
     PRIMARY_COLOR           = '#66BEFD',
     LIGHT_GREY              = '#CFCFCF',
+
+    // BUTTON COLORS 
     LOGIN_BUTTON_COLOR      = '#5cb85c',
     LOGOUT_BUTTON_COLOR     = '#d9534f',
     SIGNUP_BUTTON_COLOR     = '#0275d8',
+
+    // OTHER CONSTANTS
+
 }
