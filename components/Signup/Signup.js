ebce992dac8b4cd4ec4d1583a73b0c0e17c61795
Justynpollard1 <jmp447@usask.ca>
2020-11-16 00:16:15 -0600

    Updated googleSignIn method
    Commented out firebase google sign in

--- components/Signup/Signup.js
+++ components/Signup/Signup.js
@@ -3,10 +3,8 @@ import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import { PRIMARY_COLOR } from "../../constants/colors";
 import * as Google from 'expo-google-app-auth';
-import firebase from '../..firebase/firebase.js';
+//import firebase from '../../firebase/Fire';
 
-var provider = new firebase.auth.GoogleAuthProvider()
-firebase.auth().languageCode = 'pt';
 
 export default class Signup extends React.Component{
   async signInWithGoogleAsync() {
@@ -15,9 +13,29 @@ export default class Signup extends React.Component{
       scopes:['profile', 'email']});
 
     if (type === 'success') {
-      user1 = user;
+      var token = accessToken;
+      var userInfo = user;
     }
+
+    //firebase.auth().signInWithPopup(provider).then(function(result) {
+    //  // This gives you a Google Access Token. You can use it to access the Google API.
+     // var token = result.credential.accessToken;
+     // // The signed-in user info.
+     // var user = result.user;
+    //  // ...
+    //}).catch(function(error) {
+    //  // Handle Errors here.
+    //  var errorCode = error.code;
+    //var errorMessage = error.message;
+    //  // The email of the user's account used.
+     // var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+    //  var credential = error.credential;
+      // ...
+   // });
   }
+
+
 
 
   render(){
