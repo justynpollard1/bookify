23f12020abff80734aea83c01661ebb53d8792a4
Elimists <pandey.pran@gmail.com>
2020-11-19 12:54:33 -0600

    Added partial image displaying capability to screen
    After the user picks the image, it is show on the screen. Still need to add styling.

--- views/PostView/PostPageA.js
+++ views/PostView/PostPageA.js
@@ -6,73 +6,110 @@ import {
   View,
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
-  Keyboard
+  Keyboard,
+  Text,
+  Modal
 } from 'react-native';
 
 import FnGTextBoxPrimary from '../../components/TextBoxes/FnGWideTextBox'
+import FnGTextBoxPrimaryT from '../../components/TextBoxes/FnGWideTextBox'
 import FnGShortTextBox from '../../components/TextBoxes/FnGShortTextBox'
 import FnGAddBookImageButton from '../../components/Buttons/FnGAddBookImageButton'
 import FnGButton from '../../components/Buttons/FnGButton'
 
-const  PostPageA = () =>  {
-  const [bookTitle, setBookTitle] = useState()
-  const [bookAuthor, setBookAuthor] = useState()
-  const [bookDescription, setBookDescription] = useState()
+const  PostPageA = (props) =>  {
+  const [bookTitle, setBookTitle] = useState('')
+  const [bookAuthor, setBookAuthor] = useState('')
+  const [bookDescription, setBookDescription] = useState('')
+  const [imageA, setImageA] = useState(null)
+  const [imageB, setImageB] = useState(null)
+  const [imageC, setImageC] = useState(null)
   const [keyboardAvoider, setKeyboardAvoider] = useState(false)
+
+  const [isBookTitleEmpty, setBookTitleEmptyStatus] = useState(true)
+  const [isBookAuthorEmpty, setBookAuthorEmptyStatus] = useState(true)
+  const [isBookDescriptionEmpty, setBookDescriptionEmptyStatus] = useState(true)
+  /*const checkBookTitle = () => {
+    if (bookTitle.trim() == 0){
+      alert('Please Enter Book Title')
+      return
+    }
+    setBookTitleEmptyStatus(false)
+    alert('Success')
+  }*/
   
+
+ 
+
   return (
+    <View>
     <KeyboardAvoidingView 
-    
       behavior="position" 
       enabled={keyboardAvoider}
       >
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <SafeAreaView style={styles.container}>
-      <FnGAddBookImageButton
-        onPress={() => Alert.alert('Adding Image Pressed')}>
+          <FnGAddBookImageButton
+            onPress={(val) => setImageA(val.type)}>
 
-      </FnGAddBookImageButton>
-      <FnGTextBoxPrimary 
-        label="What is the name of your book?" 
-        placeholder="Title" 
-        multiline={false}
-        onChangeText={(val) => setBookTitle(val)}
-        onFocus={() => setKeyboardAvoider(false)}>
-      </FnGTextBoxPrimary>
+          </FnGAddBookImageButton>
+          
+          <FnGTextBoxPrimaryT
+            label="What is the name of your book?" 
+            placeholder="Title" 
+            multiline={false}
+            onChangeText={(val) => {
+              setBookTitle(val)}}
+            onFocus={() => setKeyboardAvoider(false)}
+            
+            >
+          </FnGTextBoxPrimaryT>
+          
+            {/*<View>{(errorCheckBookTitle) ? <Text>{bookTitle}</Text> : <Text>Errors</Text>}</View>*/}
+        
+          <FnGTextBoxPrimary
+            label="Who is the author of the book?" 
+            placeholder="Author" 
+            multiline={false}
+            onChangeText={(val) => setBookAuthor(val)}
+            onFocus={() => setKeyboardAvoider(true)}>
+          </FnGTextBoxPrimary>
 
-      <FnGTextBoxPrimary 
-        label="Who is the author of the book?" 
-        placeholder="Author" 
-        multiline={false}
-        onChangeText={(val) => setBookAuthor(val)}
-        onFocus={() => setKeyboardAvoider(true)}>
-      </FnGTextBoxPrimary>
+          <FnGTextBoxPrimary 
+            label="Tell us a bit about your book." 
+            placeholder="Book Description" 
+            multiline={true}
+            onChangeText={(val) => setBookDescription(val)}
+            onFocus={() => setKeyboardAvoider(true)}>
+          </FnGTextBoxPrimary>
+              
+          <View style={styles.buttonAlignment}>
+            <FnGButton 
+              text="Clear All" 
+              onPress={ () => {{
+                setBookTitle('') 
+                setBookAuthor('') 
+                setBookDescription('')
+                
+                }}}
+              buttonStyle="secondary">
+            </FnGButton>
 
-      <FnGTextBoxPrimary 
-        label="Tell us a bit about your book." 
-        placeholder="Book Description" 
-        multiline={true}
-        onChangeText={(val) => setBookDescription(val)}
-        onFocus={() => setKeyboardAvoider(true)}>
-      </FnGTextBoxPrimary>
-
-      <View style={styles.buttonAlignment}>
-        <FnGButton 
-          text="Clear All" 
-          onPress={ () => Alert.alert('Cleared')}
-          buttonStyle="secondary">
-        </FnGButton>
-
-        <FnGButton 
-          text="Next" 
-          onPress={() => Alert.alert('Next Pressed')} 
-          buttonStyle="primary">
-        </FnGButton>
-      </View>
-      </SafeAreaView>
+            <FnGButton 
+              text="Next" 
+              onPress={() => props.navigation.navigate("PostPageB")} 
+              buttonStyle="primary">
+            </FnGButton>
+          </View>
+          
+              <View><Text>{imageA} {bookTitle} {bookAuthor} {bookDescription}</Text></View>
+          
+        </SafeAreaView>
 
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
+    </View>
+  
   );
 }
 
@@ -80,13 +117,19 @@ export default PostPageA;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    
     alignItems: "center",
   },
 
   buttonAlignment:{
     flexDirection:'row',
     marginTop: 20,
+  },
+
+  modalStyle:{
+    borderRadius: 10,
+    borderWidth: 5,
+    borderColor: 'black'
   }
 });
 
